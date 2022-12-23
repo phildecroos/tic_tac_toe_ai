dce138c09b9b49e296edc78387f07fc00d00bd51
Philip DeCroos <decroosphil@gmail.com>
2022-12-23 17:22:00 -0500

    network calculations bug fix

--- network.py
+++ network.py
@@ -21,38 +21,40 @@ class Node:
 # generate network objects and read in parameters
 def generate():
     nodes = [[] for i in range(9)]
+    layers = 3
 
     # this is the only way of initializing the list of node objects that i've 
     # found that doesn't link them to the same object in memory
-    nodes[0].append(Node(9))
-    nodes[1].append(Node(9))
-    nodes[2].append(Node(9))
-    nodes[3].append(Node(9))
-    nodes[4].append(Node(9))
-    nodes[5].append(Node(9))
-    nodes[6].append(Node(9))
-    nodes[7].append(Node(9))
-    nodes[8].append(Node(9))
-    
-    nodes[0].append(Node(9))
-    nodes[1].append(Node(9))
-    nodes[2].append(Node(9))
-    nodes[3].append(Node(9))
-    nodes[4].append(Node(9))
-    nodes[5].append(Node(9))
-    nodes[6].append(Node(9))
-    nodes[7].append(Node(9))
-    nodes[8].append(Node(9))
-
-    nodes[0].append(Node(9))
-    nodes[1].append(Node(9))
-    nodes[2].append(Node(9))
-    nodes[3].append(Node(9))
-    nodes[4].append(Node(9))
-    nodes[5].append(Node(9))
-    nodes[6].append(Node(9))
-    nodes[7].append(Node(9))
-    nodes[8].append(Node(9))
+    if layers > 0:
+        nodes[0].append(Node(9))
+        nodes[1].append(Node(9))
+        nodes[2].append(Node(9))
+        nodes[3].append(Node(9))
+        nodes[4].append(Node(9))
+        nodes[5].append(Node(9))
+        nodes[6].append(Node(9))
+        nodes[7].append(Node(9))
+        nodes[8].append(Node(9))
+    if layers > 1:
+        nodes[0].append(Node(9))
+        nodes[1].append(Node(9))
+        nodes[2].append(Node(9))
+        nodes[3].append(Node(9))
+        nodes[4].append(Node(9))
+        nodes[5].append(Node(9))
+        nodes[6].append(Node(9))
+        nodes[7].append(Node(9))
+        nodes[8].append(Node(9))
+    if layers > 2:
+        nodes[0].append(Node(9))
+        nodes[1].append(Node(9))
+        nodes[2].append(Node(9))
+        nodes[3].append(Node(9))
+        nodes[4].append(Node(9))
+        nodes[5].append(Node(9))
+        nodes[6].append(Node(9))
+        nodes[7].append(Node(9))
+        nodes[8].append(Node(9))
     
     nodes[0].append(Node(1))
     nodes[1].append(Node(1))
@@ -78,15 +80,15 @@ def get_outputs(nodes, board):
                 inputs1 = [0 for i in range(9)]
                 for l in range(9):
                     # input layer
-                    inputs1[k] = nodes[k][0].get_output(j, board[k])
+                    inputs1[l] = nodes[l][0].get_output(k, board[l])
                 # hidden layer
-                input1 = nodes[j][1].get_input(inputs1)
-                inputs2[j] += nodes[j][1].get_output(i, input1)
+                input1 = nodes[k][1].get_input(inputs1)
+                inputs2[k] += nodes[k][1].get_output(j, input1)
             # hidden layer
-            input2 = nodes[j][1].get_input(inputs2)
+            input2 = nodes[j][2].get_input(inputs2)
             inputs3[j] += nodes[j][2].get_output(i, input2)
         # output layer
-        input3 = nodes[i][2].get_input(inputs3)
+        input3 = nodes[i][3].get_input(inputs3)
         outputs[i] = nodes[i][3].get_output(0, input3)
     
     return outputs
@@ -101,33 +103,3 @@ def find_move(nodes, board):
             return max_index
         else:
             outputs[max_index] = -1.0
-
-
-
-
-# def rand_params(nodes):
-#     for i in range(len(nodes)):
-#         for j in range(len(nodes[0]) - 1):
-#             for k in range(nodes[i][j].outputs):
-#                 nodes[i][j].weights[k] = random.random() + random.randint(-1, 1)
-
-# nodes = generate()
-# for i in range(9):
-#     nodes[i][len(nodes[0]) - 1].weights[0] = 1.0
-# situations = read_situations("situations.txt")
-
-# gradient = []
-# for i in range(len(nodes)):
-#     gradient.append([])
-#     for j in range(len(nodes[0]) - 1):
-#         gradient[i].append([])
-#         for k in range(nodes[i][j].outputs):
-#             gradient[i][j].append(1)
-
-# rand_params(nodes)
-
-# board = [0,0,0,0,0,0,0,0,0]
-
-# print("\nboard" + str(board))
-# print("network outputs: " + str(get_outputs(nodes, board)))
-# print("chosen move: " + str(find_move(nodes, board)))
